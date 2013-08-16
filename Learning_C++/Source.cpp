660b4f4b2ef15a9cdadd3d5351dcb6561f30c82b
Darryl Murray <darrylmurray@sympatico.ca>
2013-08-16 14:29:04 -0400

    Refactored, organized all sections of the program into functions.
    Next steps:
    -adding in directory support
    -multithreading the entire process
    
    Signed-off-by: Darryl Murray <darrylmurray@sympatico.ca>

--- Learning_C++/Source.cpp
+++ Learning_C++/Source.cpp
@@ -2,21 +2,12 @@
 #include <string>
 #include "windows.h"
 
-int end_execution(int code)
-{
-	switch(code)
-	{
-	case 0:
-		std::cout << std::endl <<std::endl<< std::endl << "Program terminating... ";
-		break;
-	case 1:
-		std::cout << "ERROR!";
-		break;
-	}
-	system ("pause");
-	return code;
-}
-
+//DECLARATIONS
+int theEngine(std::string searchTerm, WIN32_FIND_DATA & fileStruct, HANDLE & fileHandle, const std::string &directoryInput);
+int initialSearch(const std::string & searchTerm, WIN32_FIND_DATA & fileStruct, HANDLE & fileHandle);
+int fileDeletion (const std::string & directoryInput, WIN32_FIND_DATA & fileStruct, const HANDLE & fileHandle, unsigned short & confirmDelete);
+int theNextFile (WIN32_FIND_DATA & fileStruct, const HANDLE & fileHandle);
+int end_execution(int code);
 
 int main()
 {
@@ -37,59 +28,106 @@ int main()
 
 
 	WIN32_FIND_DATA fileStruct; //contains the file/directory currently under observation
+	HANDLE fileHandle;
 
-	bool exitCheck=false;
-	HANDLE fileHandle = WIN32::FindFirstFile(searchTerm.c_str(), &fileStruct); //intial file search
+	int errorCode = theEngine(searchTerm,fileStruct,fileHandle,directoryInput);
+
+	return end_execution(errorCode);
+}
+
+int theEngine(std::string searchTerm, WIN32_FIND_DATA & fileStruct, HANDLE & fileHandle, const std::string &directoryInput)
+{
+	int exitCheck = 0;
+	unsigned short confirmDelete = 0;
+	int errorCode = 1;
+
+	initialSearch(searchTerm, fileStruct, fileHandle);
+
+	do //iterates through the folder(s) searching for search term.
+	{
+		errorCode = fileDeletion(directoryInput, fileStruct, fileHandle, confirmDelete);
+	} while (exitCheck==0);
+
+	return errorCode;
+}
+
+int initialSearch(const std::string & searchTerm, WIN32_FIND_DATA & fileStruct, HANDLE & fileHandle)
+{
+	fileHandle = WIN32::FindFirstFile(searchTerm.c_str(), &fileStruct); //intial file search
 	if (fileHandle == INVALID_HANDLE_VALUE)
 	{
 		if (WIN32::GetLastError()==ERROR_FILE_NOT_FOUND) //nothing matched the search mask
 		{
-			std::cout << "No matching files found." << std::endl;
-			return end_execution(0);
+			return 3;
 		}
 		else
 		{
-			return end_execution(1);
+			return 1;
 		}
 	}
+	return 0;
+}
 
-	unsigned short confirmDelete=0;
-	do //iterates through the folder(s) searching for search term.
+int fileDeletion (const std::string & directoryInput, WIN32_FIND_DATA & fileStruct, const HANDLE & fileHandle, unsigned short & confirmDelete)
+{
+	if (confirmDelete!=3) //enables "yes to all" functionality to spare headaches
 	{
-		if (confirmDelete!=3) //enables "yes to all" functionality to spare headaches
+		std::cout << "Are you sure you want to delete " << fileStruct.cFileName << "? Press 1 if yes, 0 if no, 3 if yes to all." << std::endl; //output file name to be deleted
+		std::cin>>confirmDelete;
+	}
+
+	if (confirmDelete!=0)
+	{
+		char deleteTerm [256]; //preparing file name/path string for deletion
+		strcpy_s (deleteTerm, 256, directoryInput.c_str());
+		strcat_s (deleteTerm, 256, fileStruct.cFileName);
+
+		if (std::remove(deleteTerm)!=0)
 		{
-			std::cout << "Are you sure you want to delete " << fileStruct.cFileName << "? Press 1 if yes, 0 if no, 3 if yes to all." << std::endl; //output file name to be deleted
-			std::cin>>confirmDelete;
+			return 1;
 		}
+	}
+	else
+		std::cout << "File skipped." << std::endl;
 
-		if (confirmDelete!=0)
+	return theNextFile (fileStruct, fileHandle);
+}
+
+int theNextFile (WIN32_FIND_DATA & fileStruct, const HANDLE & fileHandle)
+{
+	auto nextFile = WIN32::FindNextFile(fileHandle, &fileStruct); //gets next file to examine
+	if (nextFile==0)
+	{
+		if (WIN32::GetLastError()==ERROR_NO_MORE_FILES)
 		{
-			char deleteTerm [256]; //preparing file name/path string for deletion
-			strcpy_s (deleteTerm, 256, directoryInput.c_str());
-			strcat_s (deleteTerm, 256, fileStruct.cFileName);
-
-			if (std::remove(deleteTerm)!=0)
-			{
-				return end_execution(1);
-			}
+			return 2;
 		}
 		else
-			std::cout << "File skipped." << std::endl;
-
-		auto nextFile = WIN32::FindNextFile(fileHandle, &fileStruct); //gets next file to examine
-		if (nextFile==0)
 		{
-			if (WIN32::GetLastError()==ERROR_NO_MORE_FILES)
-			{
-				std::cout << "This program is finished. No more files here to delete." << std::endl;
-				exitCheck=true;
-			}
-			else
-			{
-				return end_execution(0);
-			}
+			return 1;
 		}
-	} while (exitCheck==false);
+	}
+	return 0;
+}
 
-	return end_execution(0);
+int end_execution(int code) //Handles the translation between error code and corresponding text
+{
+	switch(code)
+	{
+	case 1:
+		std::cout << "ERROR!";
+		break;
+	case 2:
+		std::cout << "This program is finished. No more files here to delete." << std::endl;
+		code = 0;
+		break;
+	case 3:
+		std::cout << "No matching files found." << std::endl;
+		code = 0;
+		break;
+	}
+
+	std::cout << std::endl <<std::endl<< std::endl << "Program terminating... ";
+	system ("pause");
+	return code;
 }
